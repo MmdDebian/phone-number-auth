6488716955a6ce6d73287d50b3a865bfd7dcd86e
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-05-22 21:55:56 +0330

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+  .setTitle('PhoneNumber Auth API')
+  .setDescription('The login and verify otp api')
+  .setVersion('1.0')
+  .addTag('phoneNumber-auth')
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
