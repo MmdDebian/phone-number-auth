ae61d7ed5ac36783c88bff97e3504b2f71ef49a2
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-05-22 21:36:58 +0330

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { DatabaseModule } from './config/database/database.module';
+import { DatabaseModule } from './config/database.module';
 import { UserModule } from './user/user.module';
 import { SmsModule } from './sms/sms.module';
 import { OtpModule } from './otp/otp.module';
+import { ProfileModule } from './profile/profile.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ DatabaseModule ,  AuthModule , AuthModule, UserModule, SmsModule, OtpModule],
+  imports: [ 
+    ConfigModule.forRoot({isGlobal : true}),
+    DatabaseModule ,  
+    AuthModule, 
+    UserModule, 
+    SmsModule, 
+    OtpModule, 
+    ProfileModule ,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
